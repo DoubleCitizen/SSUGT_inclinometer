46be98093905d0cbcb1a049907ee0c4dacf8cc98
DoubleCitizen <500maxim005@gmail.com>
2024-07-13 15:48:43 +0700

    Добавлен таймер и режим статики

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,9 +1,11 @@
 import cv2
 import threading
 
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMainWindow
 
 from classes.GlobalController import GlobalController
+from classes.GlobalVarialbles import GlobalVariables
 from classes.NivelTool import NivelTool
 from classes.coordinate_system_offset import CoordinateSystemOffset
 from classes.stream_controller import StreamController
@@ -17,6 +19,7 @@ class Ui_MainWindowController(QMainWindow, main.Ui_MainWindow):
         super(Ui_MainWindowController, self).__init__()
         self.setupUi(self)
         self.segmentation: StreamController | None = None
+        self.disable_mode_static()
         GlobalController.set_checkBox_segmentaion_show(self.checkBox_segmentaion_show)
         GlobalController.set_checkBox_view_points(self.checkBox_view_points)
         GlobalController.set_checkBox_enable_record(self.checkBox_enable_record)
@@ -24,6 +27,49 @@ class Ui_MainWindowController(QMainWindow, main.Ui_MainWindow):
         GlobalController.set_spinBox_points(self.spinBox_points)
         GlobalController.set_lineEdit_source_video(self.lineEdit_source_video)
         GlobalController.set_checkBox_start_position(self.checkBox_start_position)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.onTimeout)
+
+    def start_timer(self):
+        self.timer.start(1000)  # Start the timer with 1 second interval
+
+    def stop_timer(self):
+        self.timer.stop()
+
+    def onTimeout(self):
+        GlobalVariables.add_time_static(1)
+        text_time = GlobalVariables.get_str_time_static()
+        self.label_timer.setText(text_time)
+
+    def enable_mode_static(self):
+        self.frame_static_1.setVisible(True)
+        self.frame_static_2.setVisible(True)
+
+    def disable_mode_static(self):
+        self.frame_static_1.setVisible(False)
+        self.frame_static_2.setVisible(False)
+
+    def switch_mode_static(self):
+        if self.action_static_mode.isChecked():
+            self.enable_mode_static()
+        else:
+            self.disable_mode_static()
+
+    def start_time_point(self):
+        self.start_timer()
+        self.pushButton_time_point_end.setEnabled(True)
+        self.pushButton_time_point_start.setEnabled(False)
+        GlobalVariables.set_indicator_value(False)
+
+    def stop_time_point(self):
+        self.stop_timer()
+        GlobalVariables.set_time_static(0)
+        self.pushButton_time_point_end.setEnabled(False)
+        self.pushButton_time_point_start.setEnabled(True)
+        GlobalVariables.set_indicator_value(self.lineEdit_indicator_value.text())
+
+    def update_indicator_value(self):
+        GlobalVariables.set_indicator_value(self.lineEdit_indicator_value.text())
 
     def closeEvent(self, event):
         # Здесь можно выполнить необходимые действия перед закрытием
@@ -48,9 +94,11 @@ class Ui_MainWindowController(QMainWindow, main.Ui_MainWindow):
         self.pushButton_apply_source.clicked.connect(lambda: self.apply_source())
         self.pushButton_stop_stream.clicked.connect(lambda: self.stop_stream())
         self.pushButton_start_position.clicked.connect(lambda: CoordinateSystemOffset.apply_start_position())
+        self.pushButton_time_point_start.clicked.connect(lambda: self.start_time_point())
+        self.pushButton_time_point_end.clicked.connect(lambda: self.stop_time_point())
+        self.lineEdit_indicator_value.textChanged.connect(self.update_indicator_value)
 
         self.add_actions()
-        # self.checkBox_enable_record.clicked.connect(lambda self.)
 
     def add_actions(self):
         NivelTool.set_action_nivel_220(self.menu_Nivel_220)
@@ -58,6 +106,7 @@ class Ui_MainWindowController(QMainWindow, main.Ui_MainWindow):
         self.menu_tools.aboutToShow.connect(self.update_list_com_ports)
         self.action_dialog_regres.triggered.connect(self.open_dialog_parameters_reg)
         self.action_esp32.triggered.connect(self.open_dialog_esp32)
+        self.action_static_mode.triggered.connect(self.switch_mode_static)
 
     def open_dialog_esp32(self):
         dialog = Esp32Dialog()
